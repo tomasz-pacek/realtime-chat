a0c52c855556a04d0411d0d08d79abe8f385869c
nieukki <tomekpacek@o2.pl>
2025-12-15 14:27:18 +0100

    whole realtime app commit

---
+++ src/components/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RealtimeProvider } from "@upstash/realtime/client";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <RealtimeProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RealtimeProvider>
+  );
+};
---
+++ src/lib/realtime.ts
@@ -0,0 +1,25 @@
+import * as z from "zod";
+import { InferRealtimeEvents, Realtime } from "@upstash/realtime";
+import { redis } from "./redis";
+
+const message = z.object({
+  id: z.string(),
+  sender: z.string(),
+  text: z.string(),
+  timestamp: z.number(),
+  roomId: z.string(),
+  token: z.string().optional(),
+});
+
+const schema = {
+  chat: {
+    message,
+    destroy: z.object({
+      isDestroyed: z.literal(true),
+    }),
+  },
+};
+
+export const realtime = new Realtime({ schema, redis });
+export type RealtimeEvents = InferRealtimeEvents<typeof realtime>;
+export type Message = z.infer<typeof message>;
