d746e5f53e7d330bd19d4aa56d73c3086b072f80
nieukki <tomekpacek@o2.pl>
2025-12-16 14:16:23 +0100

    elysia client fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,9 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("localhost:3000").api;
+export const client = treaty<App>(
+  "https://realtime-chat-pi-drab.vercel.app"
+).api;
+
+//https://realtime-chat-pi-drab.vercel.app
+//localhost:3000
