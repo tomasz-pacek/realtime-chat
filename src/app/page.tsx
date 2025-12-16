31ff0837f9a6497af07254c53e19827b86b5433a
Tomasz Pacek <64228829+tomasz-pacek@users.noreply.github.com>
2025-12-16 14:44:43 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ function Lobby() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8">
-        {errorMins.length > 0 && errorMins !== "" && (
+        {errorMins !== "" && (
           <div className="bg-red-950/50 border border-red-900 p-4 text-center">
             <p className="text-red-500 text-sm font-bold">
               CAN&apos;T CREATE A ROOM
